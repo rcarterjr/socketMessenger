1746728ab92f6aac4aef125c22d8e12ed73c18c3
Russell Carter <russell.c12@gmail.com>
2019-02-19 23:34:33 -0500

    joined io.on blocks

--- index.js
+++ index.js
@@ -3,19 +3,12 @@ var http = require('http').Server(app)
 var io = require('socket.io')(http)
 
 app.get('/', function(req, res) {
-  // res.send('<h1>Hello World</h1>)
+  //res.send('<h1>Hello World</h1>')
   res.sendFile(__dirname + '/index.html')
 })
 
 http.listen(3000, function() {
   console.log('listening on *:3000')
-})
-
-io.on('connection', function(socket) {
-  console.log('a user connected') // logged each time a new user accesses localhost:3000
-  socket.on('disconnect', function() {
-    console.log('user disconnected') // logged when a user leaves localhost:3000
-  })
 })
 
 // io.emit('some event', { for: 'everyone' }) // io.emit () sends some event to everyone
@@ -24,9 +17,15 @@ io.on('connection', function(socket) {
 //   socket.broadcast.emit('hi') // excludes a socket from a broadcast
 // })
 
+var solution = 'hello'
+
 io.on('connection', function(socket) {
   socket.on('chat message', function(msg) {
     io.emit('chat message', msg)
     console.log('message: ' + msg)
+    console.log('a user connected') // logged each time a new user accesses localhost:3000
+    socket.on('disconnect', function() {
+      console.log('user disconnected') // logged when a user leaves localhost:3000
+    })
   })
 })
